55e46f12642832a46d6ad60952a3eb99246c4b05
Manul24 <43578981+Manul24@users.noreply.github.com>
2018-10-09 22:32:41 +0700

    Add files via upload

---
+++ InputDataMahasiswa/Latihanwinform/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Latihanwinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mskNPM = new System.Windows.Forms.MaskedTextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.rdoLakilaki = new System.Windows.Forms.RadioButton();
+            this.rdoPerempuan = new System.Windows.Forms.RadioButton();
+            this.btnTampil = new System.Windows.Forms.Button();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTempatLahir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Jenis Kelamin";
+            // 
+            // mskNPM
+            // 
+            this.mskNPM.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskNPM.Location = new System.Drawing.Point(131, 15);
+            this.mskNPM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mskNPM.Mask = "##.##.####";
+            this.mskNPM.Name = "mskNPM";
+            this.mskNPM.Size = new System.Drawing.Size(141, 25);
+            this.mskNPM.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(131, 54);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(239, 25);
+            this.txtNama.TabIndex = 2;
+            // 
+            // rdoLakilaki
+            // 
+            this.rdoLakilaki.AutoSize = true;
+            this.rdoLakilaki.Checked = true;
+            this.rdoLakilaki.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoLakilaki.Location = new System.Drawing.Point(131, 97);
+            this.rdoLakilaki.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoLakilaki.Name = "rdoLakilaki";
+            this.rdoLakilaki.Size = new System.Drawing.Size(80, 21);
+            this.rdoLakilaki.TabIndex = 3;
+            this.rdoLakilaki.TabStop = true;
+            this.rdoLakilaki.Text = "Laki-laki";
+            this.rdoLakilaki.UseVisualStyleBackColor = true;
+            // 
+            // rdoPerempuan
+            // 
+            this.rdoPerempuan.AutoSize = true;
+            this.rdoPerempuan.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoPerempuan.Location = new System.Drawing.Point(265, 97);
+            this.rdoPerempuan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoPerempuan.Name = "rdoPerempuan";
+            this.rdoPerempuan.Size = new System.Drawing.Size(96, 21);
+            this.rdoPerempuan.TabIndex = 3;
+            this.rdoPerempuan.Text = "Perempuan";
+            this.rdoPerempuan.UseVisualStyleBackColor = true;
+            // 
+            // btnTampil
+            // 
+            this.btnTampil.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTampil.Location = new System.Drawing.Point(16, 233);
+            this.btnTampil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTampil.Name = "btnTampil";
+            this.btnTampil.Size = new System.Drawing.Size(84, 30);
+            this.btnTampil.TabIndex = 4;
+            this.btnTampil.Text = "Tampil";
+            this.btnTampil.UseVisualStyleBackColor = true;
+            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTutup.Location = new System.Drawing.Point(287, 233);
+            this.btnTutup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(84, 30);
+            this.btnTutup.TabIndex = 4;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            this.btnTutup.Click += new System.EventHandler(this.btnTutup_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tempat Lahir";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtTempatLahir
+            // 
+            this.txtTempatLahir.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempatLahir.Location = new System.Drawing.Point(131, 138);
+            this.txtTempatLahir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTempatLahir.Name = "txtTempatLahir";
+            this.txtTempatLahir.Size = new System.Drawing.Size(239, 25);
+            this.txtTempatLahir.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 182);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tanggal Lahir";
+            this.label5.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dtpTanggalLahir
+            // 
+            this.dtpTanggalLahir.CustomFormat = "dd/MM/yyyy";
+            this.dtpTanggalLahir.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpTanggalLahir.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(131, 175);
+            this.dtpTanggalLahir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpTanggalLahir.Name = "dtpTanggalLahir";
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(239, 25);
+            this.dtpTanggalLahir.TabIndex = 7;
+            this.dtpTanggalLahir.Value = new System.DateTime(2018, 10, 1, 0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 292);
+            this.Controls.Add(this.dtpTanggalLahir);
+            this.Controls.Add(this.txtTempatLahir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.btnTampil);
+            this.Controls.Add(this.rdoPerempuan);
+            this.Controls.Add(this.rdoLakilaki);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.mskNPM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Input Data Mahasiswa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox mskNPM;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.RadioButton rdoLakilaki;
+        private System.Windows.Forms.RadioButton rdoPerempuan;
+        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTempatLahir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
+    }
+}
+
